4e3cb963eb02c37270e8036dc9cb2fd86cb5de0a
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-16 16:52:17 +0100

    remove abstracted files

--- lib/pipe_dream/web/controllers/elm_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PipeDream.Web.ElmController do
-  use PipeDream.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
--- lib/pipe_dream/web/router.ex
+++ lib/pipe_dream/web/router.ex
@@ -15,8 +15,6 @@ defmodule PipeDream.Web.Router do
 
   scope "/", PipeDream.Web do
     pipe_through :browser # Use the default browser stack
-
-    get "/", ElmController, :index
   end
 
   # Other scopes may use custom stacks.
